efc3559ea0cb13c323d1137d570ffb2b4795e0e1
Kond <peter.tamas@greenfoxacademy.com>
2020-09-10 15:05:11 +0300

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import { TodoList } from './components/TodoList'
 import { AddTodo } from './components/AddTodo'
+import { useTodo } from './useTodo'
 
 function App() {
-  const [todos, setTodos] = useState([])
-
-  const newTodo = (todo) => {
-    setTodos([...todos, {...todo, id: todos.length}])
-  }
-
-  const handleDone = (id) => {
-    const todo = todos.filter(t => t.id == id)[0]
-    todo.isDone = !todo.isDone
-    setTodos([...todos.map(t => t.id != id ? t : todo)])
-  }
+  const [newTodo, todos, handleDone] = useTodo();
 
   return (
     <div className="App">
